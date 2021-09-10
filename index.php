614ee6158d402ce7e03673db33b8b7378d9a658e
Jack Tsang <jacktsang@yedpay.com>
2021-09-10 11:43:05 +0800

    EP-27-support-checkout-domain-id

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name: Yedpay for WooCommerce
  * Plugin URI: https://wordpress.org/plugins/yedpay-for-woocommerce
  * Description: Easily accept Alipay, AlipayHK, Wechat Pay, UnionPay, Visa and mastercard on your Wordpress site using Yedpay WooCommerce payment gateway in one plugin for free.
- * Version: 1.1.1
+ * Version: 1.1.2
  * Author: Yedpay
  * Author URI: https://www.yedpay.com/
  * License: GPLv2 or later
@@ -12,7 +12,7 @@
  * Text Domain: yedpay-for-woocommerce
  * Domain Path: /languages
  * WC requires at least: 3.0.0
- * WC tested up to: 5.5.2
+ * WC tested up to: 5.6.0
  */
 
 if (!defined('ABSPATH')) {
